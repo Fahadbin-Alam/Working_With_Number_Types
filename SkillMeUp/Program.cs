da95ccceef7ad6e83371d78218339137c65f5665
Fahadbin Alam <Fahadbin.Alam@yahoo.com>
2025-01-22 21:26:54 -0500

    Worked on formulas

---
+++ SkillMeUp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.VisualBasic;
+
+namespace SkillMeUp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        Console.WriteLine("Hello, World!");
+
+        int year = 1965;
+        double miles = 19534.6;
+        Console.WriteLine(year.ToString());
+        Console.WriteLine(miles.ToString());
+        */
+
+/*
+        int number1 = 10;
+        int number2 = 10;
+        double number3 = 10.0;
+        double number4 = 5.2345;
+
+        int addTwoNumbers = number1 + number2;
+        var addTwoNumbers2 = number1 + number2;
+        Console.WriteLine(addTwoNumbers);
+        Console.WriteLine(addTwoNumbers2);
+
+        number1 = 2;
+        number3 = 14.875;
+        double noValue;
+        noValue = 1.0;
+
+        Console.WriteLine(noValue);
+*/  
+    //These are the Formulas.
+    /*
+        int result1 = number1 * number2;
+        double result2 = number3 * number4;
+        var result3 = number2 / number1;
+        var result4 = number3 / number4;
+        var result5 = number1 * number4;
+        var result6 = number3 / number1;
+
+        Console.WriteLine("Result1: " + result1);
+        Console.WriteLine("Result2: " + result2);
+        Console.WriteLine("Result3: " + result3);
+        Console.WriteLine("Result4: " + result4);
+        Console.WriteLine("Result5: " + result5);
+        Console.WriteLine("Result6: " + result6);
+    */
+
+    /*
+        string namePrompt = "Please enter your name: ";
+        Console.WriteLine(namePrompt);
+
+        var name = Console.ReadLine();
+        Console.WriteLine("Hello " + name);
+        
+        var favoriteColorPrompt = "Please enter your favorite Color: ";
+        Console.WriteLine(favoriteColorPrompt);
+
+        var MyColor = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is: {MyColor}");
+    */
+
+        var carData =  "1964|Ford|Mustand|75231.9|Blue";
+        Console.WriteLine($"Length: {carData.Length}");
+
+        Cnos
+
+    }
+}
